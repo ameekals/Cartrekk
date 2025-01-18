68f725e5e5e887d9ea134e1d5e8fe7e29f4f6a45
Ameek Singh <am33ksingh@gmail.com>
2025-01-18 00:12:35 -0800

    Initial Commits

---
+++ Cartrekk/Cartrekk/CartrekkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartrekkApp.swift
+//  Cartrekk
+//
+//  Created by Ameek Singh on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CartrekkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cartrekk/CartrekkTests/CartrekkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CartrekkTests.swift
+//  CartrekkTests
+//
+//  Created by Ameek Singh on 1/18/25.
+//
+
+import Testing
+@testable import Cartrekk
+
+struct CartrekkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
