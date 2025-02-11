6efd22222903cc35c214a4d13557e775fb7b337d
Ameek Singh <am33ksingh@gmail.com>
2025-02-10 23:15:12 -0800

    Added URL Scheme for Google Sign In

--- Cartrekk/Cartrekk/CartrekkApp.swift
+++ Cartrekk/Cartrekk/CartrekkApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import GoogleSignIn
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -24,6 +25,9 @@ struct CartrekkApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .onOpenURL { url in
+                GIDSignIn.sharedInstance.handle(url)
+            }
         }
     }
 }
